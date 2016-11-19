2266eb5b96023501f9d68f77cf934f05ff4f96df
Gage Choat <gagechoat@gmail.com>
2016-11-19 12:54:19 -0800

    finished lesson 143

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { routing } from './app.routing';
@@ -38,6 +38,7 @@ import { RecipeStartComponent } from './recipes/recipe-start.component';
   imports: [
     BrowserModule,
     FormsModule,
+    ReactiveFormsModule,
     HttpModule,
     routing
   ],
